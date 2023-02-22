feaebdff9fb9234bad01327ea5c7ccb2ceb2eb82
Adhurim Canhasi <adhurimc@gmail.com>
2023-02-22 17:31:36 +0100

    portfolio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ========================= typing animation==========================*/
+
+var typed = new Typed(".typing", {
+  strings: ["Web Designer", "Web Developer", "Programmer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
