7d1b0ab1af43b8067dc943d9c8b4ff88297826b0
AdhurimCanhasi <adhurimcanhasi6@gmail.com>
2024-10-29 10:22:21 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* ========================= typing animation==========================*/
 
 var typed = new Typed(".typing", {
-  strings: ["Web Developer", "Programmer"],
+  strings: ["Web", "Web Developer", "Programmer", "Web Developer"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true,
