f0978506498ce48ae1fb21bf6213f3502a371611
Adhurim Canhasi <adhurimc@gmail.com>
2025-03-27 14:54:21 +0100

    New journals for 19,20,24 and 27 of March

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ var typed = new Typed(".typing", {
   BackSpeed: 60,
   loop: true,
 });
+// Get all sidebar links
+const navLinks = document.querySelectorAll(".nav li a");
+
+// Function to remove active class from all links
+function removeActiveClass() {
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+}
+
+// Add click event listener to each link
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    removeActiveClass(); // Remove active class from all links
+    this.classList.add("active"); // Add active class to the clicked link
+  });
+});
